2f80a61260ed82c0ad11a71c45069f670092bfeb
hiverkiya <hiverkiya@gmail.com>
2022-05-13 23:08:28 +0530

    Finalized the CLI

--- reddit.mjs
+++ reddit.mjs
@@ -9,9 +9,9 @@ const { argv } = yargs(process.argv)
 // init fetch to reddit api
 const res = await fetch('https://www.reddit.com/.json')
 const data = await res.json()
-const randomIndex = Math.floor(Math.random() * data.data.children.length)
+const randomIndexOfPost = Math.floor(Math.random() * data.data.children.length)
 // get radom post from reddit api response of all posts on front page
-const post = data.data.children[randomIndex]
+const post = data.data.children[randomIndexOfPost]
 
 // log if --print flag is passed
 if (argv.print) {
